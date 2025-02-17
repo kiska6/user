104bfa69d399b00703f0f15d2ba1b72b409589f9
kiska6 <166571073+kiska6@users.noreply.github.com>
2025-02-17 20:03:04 +0800

    Update

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 window.routerBase = 'https://in.dianying.my/'
 window.settings = {
   // 站点名称
-  title: '电影',
+  title: '电影云',
   // 站点描述
   description: '永久导航：电影.biz',
   assets_path: '/assets',
@@ -13,7 +13,7 @@ window.settings = {
   // 版本号
   version: '0.1.1-dev',
   // 登陆背景
-  background_url: 'https://bing.img.run/rand.php',
+  background_url: 'https://api.xsot.cn/bing?jump=true',
   // 站点LOGO
   logo: '',
 }
